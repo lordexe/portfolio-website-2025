282327e7021bc4e4c5903106e6d7c1ced897ab97
Ani Chauhan <thechauhananirudh@gmail.com>
2025-09-29 15:04:47 -0500

    Added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Ani Chauhan - Designer, Animator, Developer",
   description: "Ani Chauhan's Portfolio - Designer, Animator, Developer",
+  icons: {
+    icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({
